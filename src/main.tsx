23c5a3d8ecd1b282080429f308da6d579b272409
eng-raghadsami <raghadsami.w@gmail.com>
2026-01-09 20:08:28 +0200

    feat(ui): create app entry and base styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./main.css";
 import App from "./App.tsx";
+
+import "./style/index.css";
 
 const queryClient = new QueryClient();
 
@@ -11,5 +12,5 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </StrictMode>,
+  </StrictMode>
 );
