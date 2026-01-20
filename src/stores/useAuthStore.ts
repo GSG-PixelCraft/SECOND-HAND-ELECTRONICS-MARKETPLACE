7db57d301523e59b1ff0ccda552c4a068662f597
Shadi Sbaih <shadisbaih20014@gmail.com>
2026-01-20 16:28:21 +0200

    chore: remove Redux store and slices, migrate to Zustand for state management

---
+++ src/stores/useAuthStore.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+
+import type { User } from "../types";
+
+interface AuthState {
+  user: User | null;
+  token: string | null;
+  setUser: (user: User | null) => void;
+  setToken: (token: string | null) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  token: null,
+  setUser: (user: User | null) => {
+    set({ user });
+  },
+  setToken: (token: string | null) => {
+    set({ token });
+  },
+  logout: () => {
+    set({ user: null, token: null });
+  },
+}));
