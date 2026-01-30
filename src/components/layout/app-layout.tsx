a997f953e6b6947e61c478a10cb9aae46ea28771
Shadi Sbaih <shadisbaih20014@gmail.com>
2026-01-30 17:32:42 +0200

    fix issues

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -9,14 +9,12 @@
 // فقط هيكل
 import { Outlet } from "react-router-dom";
 import { Header } from "./header";
-import { Sidebar } from "./sidebar";
 import { Footer } from "./footer";
 
 export const AppLayout = () => (
   <div className="flex min-h-screen flex-col bg-slate-50">
     <Header />
     <div className="flex flex-1">
-      <Sidebar />
       <main className="flex-1 px-6 py-8">
         <Outlet />
       </main>
